1c02fb1994452aa7650b90d3f1dcd3905f807bb1
DishaS31 <ds2161205@gamil.com>
2025-12-30 22:02:58 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import { Routes, Route } from "react-router"
 import Layout from "./components/Layout"
 import Home from './pages/Home'
-import Contact from './pages/contact'
+// import Contact from './pages/contact'
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
+        {/* <Route path='/contact' element={<Contact />} /> */}
       </Route>
     </Routes>
     </div>
